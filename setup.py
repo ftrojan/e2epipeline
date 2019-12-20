dfcbb3871a9e04288c4ff798ba22d74e48020f2d
filip_trojan <Tarantula2018>
2019-12-20 19:02:53 +0100

    setup.py improved

--- setup.py
+++ setup.py
@@ -3,15 +3,9 @@ from setuptools import setup
 setup(
    name='e2epipeline',
    version='1.0.0',
-   description='A useful module',
+   description='Generalisation of sklearn Pipeline to allow for more flexible mapping of input and output parameters',
    author='Filip Trojan',
    author_email='f.trojan@centrum.cz',
-   packages=['e2epipeline'],  #same as name
-   install_requires=['numpy', 'pandas'], #external packages as dependencies
-   scripts=[
-            'examples/example1',
-            'examples/example2',
-            'examples/example3',
-            'examples/example_rename'
-           ]
+   packages=['e2epipeline'],
+   install_requires=['numpy', 'pandas'],
 )
