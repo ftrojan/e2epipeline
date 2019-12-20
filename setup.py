4beaec39c95683a81617f8666ab235048da6daa0
filip_trojan <Tarantula2018>
2019-12-20 19:08:22 +0100

    package_dir in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-   name='e2epipeline',
-   version='1.0.0',
-   description='Generalisation of sklearn Pipeline to allow for more flexible mapping of input and output parameters',
-   author='Filip Trojan',
-   author_email='f.trojan@centrum.cz',
-   packages=['e2epipeline'],
-   install_requires=['numpy', 'pandas'],
+    name='e2epipeline',
+    version='1.0.0',
+    description='Generalisation of sklearn Pipeline to allow for more flexible mapping of input and output parameters',
+    author='Filip Trojan',
+    author_email='f.trojan@centrum.cz',
+    packages=['e2epipeline'],
+    package_dir={'': 'e2epipeline'},
+    install_requires=['numpy', 'pandas'],
 )
