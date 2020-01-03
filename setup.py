ad3b9fdde3c1ec5bc3fa60a0fdaac601593adf68
filip_trojan <Tarantula2018>
2020-01-03 19:06:11 +0100

    1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='e2epipeline',
-    version='1.0.0',
+    version='1.0.1',
     description='Generalisation of sklearn Pipeline to allow for more flexible mapping of input and output parameters',
     author='Filip Trojan',
     author_email='f.trojan@centrum.cz',
